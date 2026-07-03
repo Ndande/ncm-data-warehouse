9bd1c62e4a0743da4037851c0258de2c082d7aba
Ndande <ndandeyezai@yahoo.com>
2026-07-03 12:33:09 +0100

    functions for loading the dim site and supplier

--- etl.py
+++ etl.py
@@ -31,13 +31,51 @@ def transform(df):
     df['U PRICE'] = pd.to_numeric(df['U PRICE'], errors='coerce')  # Convert to numeric, set errors to NaN
     df_clean = df.dropna(subset=['SITE', 'SUPPLIER', 'T PRICE'])  # Drop rows with NaN in critical columns
     return df_clean
+
+def load_dim_site(df, conn):
+    cursor = conn.cursor()
+    #Get unique sites from the dataframe
+    sites = df['SITE'].dropna().unique()
+    #Insert each site if it does not exisit
+    for site in sites:
+        cursor.execute("""
+            INSERT INTO dim_site (name)
+            VALUES (%s)
+            ON CONFLICT (name) DO NOTHING
+        """, (site,))
+    conn.commit()
+    #Build a mapping of site names to their IDs
+    cursor.execute("SELECT name, id FROM dim_site")
+    site_map = {row[0]: row[1] for row in cursor.fetchall()}
+    cursor.close()
+    return site_map
     
+def load_dim_supplier(df, conn):
+    cursor = conn.cursor()
+    #Get unique suppliers from the dataframe
+    suppliers = df['SUPPLIER'].dropna().unique()
+    #Insert each supplier if it does not exist
+    for supplier in suppliers:
+        cursor.execute("""
+            INSERT INTO dim_supplier (name)
+            VALUES (%s)
+            ON CONFLICT (name) DO NOTHING
+        """, (supplier,))
+    conn.commit()
+    #Build a mapping of supplier names to their IDs
+    cursor.execute("SELECT name, id FROM dim_supplier")
+    supplier_map = {row[0]: row[1] for row in cursor.fetchall()}
+    cursor.close()
+    return supplier_map
+
 if __name__ == "__main__":
     conn = get_connection()
     if conn:
         print("Connection successful")
         df = extract()
         df = transform(df)
-        print(df.shape)
-        print(df[['DATE', 'SITE', 'SUPPLIER', 'T PRICE']].head(3))
+        site_map = load_dim_site(df, conn)
+        supplier_map = load_dim_supplier(df, conn)
+        print(site_map)
+        print(supplier_map)
         conn.close()
